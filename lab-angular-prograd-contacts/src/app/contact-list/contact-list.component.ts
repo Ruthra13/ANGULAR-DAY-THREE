85eaa3cc2b7f6cf759242ca54cc1222d7adafe48
ruthra <ruthraworld@gmail.com>
2020-09-04 23:40:37 +0530

    lab1-contacts

---
+++ lab-angular-prograd-contacts/src/app/contact-list/contact-list.component.ts
@@ -0,0 +1,28 @@
+import { contacts } from "./../contact";
+import { userContact } from "./../userContact";
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-contact-list",
+  templateUrl: "./contact-list.component.html",
+  styleUrls: ["./contact-list.component.css"],
+})
+export class ContactListComponent implements OnInit {
+  contacts: userContact[];
+  newContact: any = {};
+  constructor() {}
+
+  ngOnInit() {
+    // initialize your contacts here
+    this.contacts = contacts;
+  }
+
+  addContact(newContact): any {
+    console.log("Add contact has been called");
+    this.contacts.push(newContact);
+
+    // Fill your code here
+    // add contact to contacts list
+    // clear inputs
+  }
+}
